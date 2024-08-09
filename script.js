e5511410c10b2125b14a46e536126ccff8b5f82c
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-08-09 00:39:05 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let princpleAmount = prompt("Enter the principle: ");
+let annualRateOfInterest = prompt("Enter the rate of interest: ");
+let numberOfYears = prompt("Enter the number of years: ");
+console.log(princpleAmount, annualRateOfInterest, numberOfYears);
