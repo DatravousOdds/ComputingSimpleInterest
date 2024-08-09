8d8267af9b608883d6d0018ec1734fb0b9a26398
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-08-09 01:12:45 +0000

    progress w simple interest lg

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let princpleAmount = prompt("Enter the principle: ");
 let annualRateOfInterest = prompt("Enter the rate of interest: ");
 let numberOfYears = prompt("Enter the number of years: ");
-console.log(princpleAmount, annualRateOfInterest, numberOfYears);
+
+annualRateOfInterest = parseFloat(annualRateOfInterest);
+console.log("rate of interest: ", annualRateOfInterest);
+let endOfInvesment = princpleAmount * (1 + annualRateOfInterest * numberOfYears)
+console.log(endOfInvesment);
+
+console.log(`After ${numberOfYears} years at ${annualRateOfInterest}% the investment will be worth ${enOfInvesment}`)
